4efa0a8009956b29c1c57e2f115df2a38b3f9a41
Mohadeseh <m.moradi6588@gmail.com>
2022-12-31 21:19:59 +0330

    Added bootstrap

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,28 +1,32 @@
 import React from "react";
 import "./WeatherApp.css";
 
+
 export default function WeatherApp() {
   return (
     <div className="WeatherApp">
-      <h1>WeatherApp</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://symphonious-sunflower-8ca288.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Mohadeseh Moradian
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/Mohad6588/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open_sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>WeatherApp</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://symphonious-sunflower-8ca288.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mohadeseh Moradian
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/Mohad6588/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open_sourced on GitHub
+          </a>
+          <button className="btn btn-primary d-block">Submit</button>
+        </footer>
+      </div>
     </div>
   );
 }
