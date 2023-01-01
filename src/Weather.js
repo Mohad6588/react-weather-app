6de01db37ff7783749915bdf10f36dc100cd91db
Mohadeseh <m.moradi6588@gmail.com>
2023-01-01 21:27:27 +0330

    Changed basic style

--- src/Weather.js
+++ src/Weather.js
@@ -13,32 +13,35 @@ export default function Weather() {
               className="form-control d-inline-block me-2 lh-2"
               autoFocus="on"
             />
-            <input type="submit" value="Search" className=" lh-2  p-1 " />
+            <input type="submit" value="Search" className="btn btn-warning searchBtn" />
           </form>
-          <h2 className="mt-3">San Francisco </h2>
-          <h4>United States of America</h4>
-          <ul>
-            <li>Saturday 11:00</li>
-            <li>Heavy intensity rain</li>
-            <li>Feels Like: 13℃</li>
-            <li>Humidity: 100%</li>
-            <li>Wind: 16km/h</li>
-          </ul>
-          <div className="row mt-3">
-            <div className="min-max col-4">
-              <ul>
-                <li>Min: 11℃</li>
-                <li>Max: 13℃</li>
-              </ul>
-            </div>
-            <div className="col-8">
-              <img
-                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png"
-                alt="rainy"
-                className="d-inline-block currentIcon "
-              />
-              <span className="currentTemp">13</span>
-              <span className="unit">°C</span>
+          <div className="currentWeather mt-3">
+            {" "}
+            <h2 className="pt-2 mb-1">San Francisco </h2>
+            <h4>United States of America</h4>
+            <ul>
+              <li>Saturday 11:00</li>
+              <li>Heavy intensity rain</li>
+              <li>Feels Like: 13℃</li>
+              <li>Humidity: 100%</li>
+              <li>Wind: 16km/h</li>
+            </ul>
+            <div className="row mt-3">
+              <div className="min-max col-4">
+                <ul>
+                  <li>Min: 11℃</li>
+                  <li>Max: 13℃</li>
+                </ul>
+              </div>
+              <div className="col-8">
+                <img
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png"
+                  alt="rainy"
+                  className="d-inline-block currentIcon "
+                />
+                <span className="currentTemp">13</span>
+                <span className="unit">°C</span>
+              </div>
             </div>
           </div>
         </div>
